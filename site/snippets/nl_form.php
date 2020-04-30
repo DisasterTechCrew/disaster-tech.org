50fcc5344d5c2495a304c696e869dc97dd9481f3
Niklas Jordan <hello@niklasjordan.com>
2020-04-30 17:23:46 +0200

    form

--- site/snippets/nl_form.php
+++ site/snippets/nl_form.php
@@ -1,9 +1,13 @@
 <div class="newsletter-form">
     <h2>Subscribe to our newsletter!</h2>
-    <div class="container">
-        <form action="">
+    <p>Add your email address to subscribe to the newsletter, summarising what's happening in the DisasterTech community. We'll only ever use your email address for this newsletter.</p>
+    <div class="form_container">
+        <form action="https://e.niklasjordan.com/subscribe" method="POST" accept-charset="utf-8">
             <input type="email" name="email" id="email" placeholder="example@example.com" />
-            <input type="submit" name="submit" value="subscribe">
+            <input type="submit" name="submit" id="submit" value="subscribe">
+            <input type="hidden" name="list" value="Ro3FQCyViGDM7NuBEAPJbg"/>
+	        <input type="hidden" name="subform" value="yes"/>
+            <label class="checkbox" for="gdpr"><input type="checkbox" id="gdpr" name="gdpr" class="gdpr-checkbox"><span>GDPR permission: We'll only ever use your email address for this newsletter. You can unsubscribe at any time by clicking the link in the footer of our emails. Are you okay with that?</span></label>
         </form>
     </div>
 </div>
