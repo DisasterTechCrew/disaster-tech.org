5196f5e284f1bc922c55f0d0a70f53894daccbb0
Niklas Jordan <hello@niklasjordan.com>
2020-05-03 22:15:13 +0200

    Form

--- site/snippets/nl_form.php
+++ site/snippets/nl_form.php
@@ -3,12 +3,12 @@
     <p>Add your email address to subscribe to the newsletter, summarising what's happening in the DisasterTech community. We'll only ever use your email address for this newsletter.</p>
     <div class="form_container">
         <form action="https://e.niklasjordan.com/subscribe" method="POST" accept-charset="utf-8">
-            <input type="email" name="email" id="email" placeholder="example@example.com" />
+            <input type="email" name="email" id="email" placeholder="example@example.com" required />
             <input type="submit" name="submit" id="submit" value="subscribe">
             <input type="hidden" name="list" value="Ro3FQCyViGDM7NuBEAPJbg"/>
             <input type="hidden" name="subform" value="yes"/>
             <div style="display: block; margin-top: 20px;">
-            <label class="checkbox" for="gdpr"><input type="checkbox" id="gdpr" name="gdpr" class="gdpr-checkbox"><span>GDPR permission: We'll only ever use your email address for this newsletter. You can unsubscribe at any time by clicking the link in the footer of our emails. Are you okay with that?</span></label>
+            <label class="checkbox" for="gdpr"><input type="checkbox" id="gdpr" name="gdpr" class="gdpr-checkbox" required><span>GDPR permission: We'll only ever use your email address for this newsletter. You can unsubscribe at any time by clicking the link in the footer of our emails. Are you okay with that?</span></label>
             </div>
         </form>
     </div>
