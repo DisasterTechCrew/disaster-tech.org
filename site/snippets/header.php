a0469174293b3bc1f2afeb6b9fde72f7598ef56a
Niklas Jordan <hello@niklasjordan.com>
2020-06-05 14:57:24 +0200

    Blog Homepage Nav Error

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -5,6 +5,7 @@
   <li><a href="#about">About us</a></li>
   <li><a href="#projects">Projects</a></li>
   <li><a href="#join">Join the crew!</a></li>
+  <li><a href="/blog">Blog</a></li>
 <?php else: ?>
   <li class="logo"><a href="https://www.disaster-tech.org/#header">DisasterTech</a></li>
   <li><a href="https://www.disaster-tech.org/#about">About us</a></li>
